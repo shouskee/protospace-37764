b6eb69271f369792f52fe0d0d0e910d091e253d8
shouskee <rummy5163@gmail.com>
2022-04-17 21:56:13 +0900

    変更しました

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,6 +1,6 @@
 class PrototypesController < ApplicationController
   before_action :authenticate_user!, only:[:new, :edit, :destroy]
-  before_action :move_to_edit, except: [:edit]
+  before_action :move_to_edit, only: [:edit]
 
   def index
     @prototypes = Prototype.all
@@ -50,8 +50,11 @@ class PrototypesController < ApplicationController
   end
 
   def move_to_edit
-    unless user_signed_in?
-      redirect_to action: :edit
+    
+    @prototype = Prototype.find(params[:id])
+    
+      unless user_signed_in? && current_user.id == @prototype.user_id
+      redirect_to action: :index
     end
   end
 end
